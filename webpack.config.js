d6da53a03f5d94f6c4f5a5ac061906a6e5b0f235
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-08-27 18:06:33 +0100

    bare-bone markup in progress

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,27 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
 };
